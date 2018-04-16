784474cd6f3ecc3095c14948ce25542da3e69adf
kisenka <qtuzov@gmail.com>
2018-04-16 18:21:35 +0300

    Update plugin meta

--- kotlin-playground-wp-plugin.php
+++ kotlin-playground-wp-plugin.php
@@ -4,8 +4,10 @@
  *
  * Plugin Name: Kotlin Playground Plugin
  * Description: Allows to embed interactive Kotlin playground to any post via <code>[kotlin]</code> shortcode.
- * Version: 0.0.1
+ * Version: 0.0.2
  * License: Apache-2.0
+ * Plugin URI: https://github.com/Kotlin/kotlin-playground-wp-plugin
+ * Author URI: https://github.com/Kotlin
  */
 
 require 'kotlin-shortcode.php';
